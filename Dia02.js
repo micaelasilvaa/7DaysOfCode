c54a7d83ce0245c29f8c8d8e567def8171bc5691
Micaela Silva <133119906+micaelasilvaa@users.noreply.github.com>
2023-06-27 11:28:33 -0300

    Add files via upload

---
+++ Dia02.js
@@ -0,0 +1,9 @@
+const nome = prompt ("Qual o seu nome?")
+console.log (nome);
+const idade = prompt ("Quantos anos você tem?")
+console.log (idade);
+const linguagem = prompt ("Qual a linguagem de programação você está estudando?")
+console.log (linguagem)
+const mensagem = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+
+alert (mensagem);
